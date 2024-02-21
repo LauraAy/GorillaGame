286c833093c03f0d10c41424758e6dedd8e5859e
laura_ay <laura.ay1564@gmail.com>
2024-02-20 16:06:51 -0800

    initial setup for gorilla game

---
+++ index.js
@@ -0,0 +1,35 @@
+// The state of the game
+let state = {};
+// ...
+
+// References to HTML elements
+const canvas = document.getElementById("game");
+// ...
+
+newGame();
+
+function newGame() {
+  // Initialize game state
+  state = {
+    // ...
+  };
+
+  // ...
+
+  draw();
+}
+
+function draw() {
+  // ...
+}
+
+// Event handlers
+// ...
+
+function throwBomb() {
+  // ...
+}
+
+function animate(timestamp) {
+  // ...
+}
